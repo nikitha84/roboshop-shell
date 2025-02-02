d39ef9bece8cff1f84296cab38a3b99252db15ff
nikitha84 <nikitha84640@gmail.com>
2025-02-02 15:43:05 +0530

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "ERROR: $2 ..installation $R failed $N"
+    else
+        echo -e "$2....installation $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "ERROR : $R Please run with root user $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copied mongoDB repo"
+
