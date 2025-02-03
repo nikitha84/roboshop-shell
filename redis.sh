ae919ecdfdd49a233d2963b6d8a62097da254c4e
nikitha84 <nikitha84640@gmail.com>
2025-02-03 14:40:25 +0530

    catalogue

--- redis.sh
+++ redis.sh
@@ -36,7 +36,7 @@ VALIDATE $? "Enabling remi relese"
 dnf install redis -y &>> $LOGFILE
 VALIDATE $? "Installing redis"
 
-sed -i 's/127.0.0.0/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $LOGFILE
 VALIDATE $? "Allowing remote access"
 
 systemctl enable redis &>> $LOGFILE
