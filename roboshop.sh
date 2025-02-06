29863c0d824a3fcef131b267924cca31fe7b0145
nikitha84 <nikitha84640@gmail.com>
2025-02-06 15:59:29 +0530

    ec2

--- roboshop.sh
+++ roboshop.sh
@@ -15,12 +15,10 @@ do
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-0af62f102bd40722d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instance[0].PrivateIpAddress' --output text)
    
-   echo "instance is: $i"
+   echo "$i: $IP_ADDRESS"
 done
 
 
 
 
 
-
-#using query cmd query data and getback ips
