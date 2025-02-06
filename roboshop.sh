949abf742e2a441a79b620bfdf185b98aa0d8435
nikitha84 <nikitha84640@gmail.com>
2025-02-06 15:33:39 +0530

    ec2

---
+++ roboshop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-0af62f102bd40722d
+INSTANCES=("mongodb" "mysql" "cart" "user")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb"] || [ $i == "mysql"]
+    then
+        INSTANCES_TYPE="t3.small"
+    else
+        INSTANCES_TYPE="t2.micro"
+    fi
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-0af62f102bd40722d
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instance[0].PrivateIpAddress' --output text)
+   
+   echo "$i: $IP_ADDRESS"
+done
+   
+
+
+
+
+
+#using query cmd query data and getback ips
