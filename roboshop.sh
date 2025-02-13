5c26cba96e7a93c7ad8cefdefcd86d2b599bafab
nikitha84 <nikitha84640@gmail.com>
2025-02-13 12:50:15 +0530

    mongo

--- roboshop.sh
+++ roboshop.sh
@@ -2,11 +2,11 @@
 
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-0af62f102bd40722d
-INSTANCES=("mongodb" "mysql" "cart" "user")
+INSTANCES=("mongodb" "mysql" "redis" "shipping" "web" "catalogue" "payment" "cart" "user")
 
 for i in "${INSTANCES[@]}"
 do
-    if [ $i == "mongodb"] || [ $i == "mysql"]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCES_TYPE="t3.small"
     else
