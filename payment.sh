9f6aa3f1c874477afcb3d755d3171acd1d93d443
nikitha84 <nikitha84640@gmail.com>
2025-02-14 13:24:13 +0530

    project

---
+++ payment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "ERROR: $2 .. $R failed $N"
+    else
+        echo -e "$2.... $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "ERROR : $R Please run with root user $N"
+    exit 1
+else
+    echo "you are root user"
+fi 
