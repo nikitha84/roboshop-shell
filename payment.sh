cbafceceb78c0008c23a49e23914479fa9091ebd
nikitha84 <nikitha84640@gmail.com>
2025-02-15 15:33:45 +0530

    new

--- payment.sh
+++ payment.sh
@@ -25,4 +25,42 @@ then
     exit 1
 else
     echo "you are root user"
+fi
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
 fi 
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating app directory"
+
+dnf install python3.11 gcc python3-devel -y &>> $LOGFILE
+VALIDATE $? "installing pythone"
+
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> $LOGFILE
+VALIDATE $? "downloading payment file"
+
+cd /app &>> $LOGFILE
+
+unzip /tmp/payment.zip &>> $LOGFILE
+VALIDATE $? "Unzing user file"
+
+pip3.11 install -r requirements.txt &>> $LOGFILE
+VALIDATE $? "installing dependencis"
+
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
+VALIDATE $? "copied payment service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "user daemon reload"
+
+systemctl enable payment &>> $LOGFILE
+VALIDATE $? "user enable payment"
+
+systemctl start payment &>> $LOGFILE
+VALIDATE $? "user start payment"
